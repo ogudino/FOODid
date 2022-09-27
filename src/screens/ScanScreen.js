68ac2f3e147ab86d225cd32be0f3013df4ba5fd5
ogudino <ogudino19@gmail.com>
2022-09-26 22:09:37 -0400

    clean scanner build

--- src/screens/ScanScreen.js
+++ src/screens/ScanScreen.js
@@ -1,56 +1,62 @@
+import * as React from 'react';
 
-import React, { useCallback, useEffect, useState } from 'react';
-import { Camera, frameRateIncluded, useCameraDevices } from 'react-native-vision-camera';
-import { Button, PermissionsAndroid, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
-import LoadingView from 'react-native-loading-view';
+import { StyleSheet, Text } from 'react-native';
+import { useCameraDevices } from 'react-native-vision-camera';
+import { Camera } from 'react-native-vision-camera';
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
 import { useIsFocused } from '@react-navigation/native';
 
-// const requestCameraPermission = async () => {
-//   try {
-//     const granted = await PermissionsAndroid.request(
-//       PermissionsAndroid.PERMISSIONS.CAMERA,
-//       {
-//         title: "Cool Photo App Camera Permission",
-//         message:
-//           "Cool Photo App needs access to your camera " +
-//           "so you can take awesome pictures.",
-//         buttonNeutral: "Ask Me Later",
-//         buttonNegative: "Cancel",
-//         buttonPositive: "OK"
-//       }
-//     );
-//     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//       console.log("You can use the camera");
-//     } else {
-//       console.log("Camera permission denied");
-//     }
-//   } catch (err) {
-//     console.warn(err);
-//   }
-// };
-
-const newCameraPermission =  Camera.requestCameraPermission()
-const cameraPermission =  Camera.getCameraPermissionStatus()
-
-
-
 const ScanScreen = ({navigation}) => {
-  const devices = useCameraDevices()
-  const device = devices.back
 
-  const isFocused = useIsFocused()
 
-  if (device == null) return <LoadingView />
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  //  const isFocused = useIsFocused();
+
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.ALL_FORMATS], {
+    checkInverted: true,
+  });
+
+  // Alternatively you can use the underlying function:
+  //
+  // const frameProcessor = useFrameProcessor((frame) => {
+  //   'worklet';
+  //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
+  //   runOnJS(setBarcodes)(detectedBarcodes);
+  // }, []);
+
+  React.useEffect(() => {
+    (async () => {
+  console.log("running")
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={isFocused}
-    />
-  )
+    device != null &&
+    hasPermission && (
+      <>
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+          frameProcessorFps={5}
+        />
+        {barcodes.map((barcode, idx) => (
+          <Text key={idx} style={styles.barcodeTextURL}>
+            {barcode.displayValue}
+          </Text>
+        ))}
+      </>
+    )
+  );
 }
 
- export default ScanScreen
+
+  export default ScanScreen
 
 const styles = StyleSheet.create({
     container: {
@@ -59,4 +65,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: "#8fcbbc",
     },
+    barcodeTextURL: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+      },
 })
